67e878d5c8c9e3d88807e170e933864d95c6e0c6
Mario <Mario.L.Geuenich@gmail.com>
2018-01-09 18:06:56 +0000

    operator precedence

--- index.js
+++ index.js
@@ -1,58 +1,62 @@
 const electron = require("electron")
 const {ipcRenderer} = electron
 
-let currentComputation = null
-// parseInt(someString) --> int
+
+let input = null
 function inputEventHandler(element) { // Handles all input except equals sign
-  currentComputation = document.getElementById("inputBox").value + element.value
-  document.getElementById("inputBox").value = currentComputation
+  input = document.getElementById("inputBox").value + element.value
+  document.getElementById("inputBox").value = input
 }
+
 
 function equalsEventHandler(element){
-  currentComputation = document.getElementById("inputBox").value
-  if (currentComputation) {
-    // logInput(currentComputation) // just to log a message that input has occurred
-    document.getElementById("output").innerHTML = parseInput(currentComputation)
-  }
-}
+  input = document.getElementById("inputBox").value
+  if (input) {
+    const pattern = /^(?:[0-9]+[*/+-])*[0-9]+/
+    input = input.match(pattern)
 
-function logInput(input) {
-  ipcRenderer.send('someAction', input);
-  return 2
-}
-
-function parseInput(input){
-  // TODO: operator precedence
-  
-  const pattern = /^(?:[0-9]+[*/+-])*[0-9]+/
-  let sum
-
-  let out = input.match(pattern)
-  if (out){
-    out = out[0]
-    let operands = out.split(/[*+-/]/) //NOTE: does it need filtering for empty str?
-    operands = operands.map(x => Number(x))
-    let operators = out.split(/[0-9]+/).filter(e => e) //filter any empty strings
-
-    sum = operands[0]
-    for (var i = 0; i < operators.length; i++){
-      switch (operators[i]) {
-        case "*":
-          sum *= operands[i+1]
-          break;
-        case "/":
-          sum /= operands[i+1]
-          break;
-        case "+":
-          sum += operands[i+1]
-          break;
-        case "-":
-          sum -= operands[i+1]
-          break;
-      }
+    if (input){
+      input = input[0]
+      let inputArray = mathStrToArray(input)
+      let rpn = toRPN(inputArray)
+      document.getElementById("output").innerHTML = calculateRPN(rpn)
     }
   }
+}
 
-  return sum
 
+function logMessage(input) { //log in Main process, renderer doesn't log to cmd
+
+
+function mathStrToArray(inputString){
+  // Manipulate math expression string so that each operand and operator is a
+  // seperate element in an Array
+
+  let operands = inputString.split(/[*+-/]/).map(x => Number(x)) //NOTE: does it need filtering like below?
+  let operators = inputString.split(/[0-9]+/).filter(e => e) //filter out any empty strings
+  let inputArrayParsed = [];
+  for (let i = 0; i < operators.length; i++) {
+    inputArrayParsed.push(operands[i]) // push = append
+    inputArrayParsed.push(operators[i])
+  };
+  let finalOperand =  operands.slice(-1, operands.length)[0] //get last element
+  inputArrayParsed.push(finalOperand);
+
+  return inputArrayParsed
+}
+
+
+function toRPN(inputArray){ // Reverse Polish Notation
+  let rpnArray = []
+
+  return rpnArray
+}
+
+
+function calculateRPN(rpnArray){
+  let computation;
+  // while (rpnArray > 2){
+  //
+  // }
+  return computation
 }
